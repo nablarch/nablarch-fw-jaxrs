a4bed7b173c7272f54f3f89e1ccf7911bc1d0a31
oguchi-shiori03 <oguchi.shiori@tis.co.jp>
2024-02-27 18:02:35 +0900

    feat: HttpRequestに処理を委譲したメソッドのassertの比較対象をHttpRequestに変更

--- src/test/java/nablarch/fw/jaxrs/JaxRsHttpRequestTest.java
+++ src/test/java/nablarch/fw/jaxrs/JaxRsHttpRequestTest.java
@@ -3,21 +3,16 @@ package nablarch.fw.jaxrs;
 import nablarch.fw.web.MockHttpCookie;
 import nablarch.fw.web.MockHttpRequest;
 import nablarch.fw.web.upload.PartInfo;
-import nablarch.fw.web.useragent.UserAgent;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsSame.sameInstance;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 /**
  * {@link JaxRsHttpRequest}のテスト。
@@ -25,252 +20,255 @@ import static org.junit.Assert.assertEquals;
 public class JaxRsHttpRequestTest {
 
     /**
-     * リクエストのメソッドを取得できることを確認
+     * HTTPリクエストと同一のメソッドを取得できることを確認
      */
     @Test
     public void testGetMethod() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
-        assertThat(sut.getMethod(), is("GET"));
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getMethod(), is(delegate.getMethod()));
     }
 
     /**
-     * HTTPリクエストURIを取得できることを確認
+     * HTTPリクエストと同一のHTTPリクエストURIを取得できることを確認
      */
     @Test
     public void testGetRequestUri() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
-        assertThat(sut.getRequestUri(), is("/"));
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getRequestUri(), is(delegate.getRequestUri()));
     }
 
     /**
-     * HTTPリクエストURIを設定できることを確認
+     * HTTPリクエストにHTTPリクエストURIを設定できることを確認
      */
     @Test
     public void testSetRequestUri() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         sut.setRequestUri("/requestUri");
-        assertThat(sut.getRequestUri(), is("/requestUri"));
+        assertThat(sut.getRequestUri(), is(delegate.getRequestUri()));
     }
 
     /**
-     * HTTPリクエストURIのパス部分が取得できることを確認
+     * HTTPリクエストと同一のHTTPリクエストURIのパス部分が取得できることを確認
      */
     @Test
     public void testGetRequestPath() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest()
-                .setRequestUri("/requestPath?paramName1=param1&paramName2=param2"));
-        assertThat(sut.getRequestPath(), is("/requestPath"));
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest().setRequestUri("/requestPath?paramName1=param1&paramName2=param2");
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getRequestPath(), is(delegate.getRequestPath()));
     }
 
     /**
-     * リクエストパスを設定できることを確認
+     * HTTPリクエストにリクエストパスを設定できることを確認
      */
     @Test
     public void testSetRequestPath() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest()
-                .setRequestUri("/requestUri?paramName1=param1&paramName2=param2"));
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest().setRequestUri("/requestPath?paramName1=param1&paramName2=param2");
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         sut.setRequestPath("/requestPath");
-        assertThat(sut.getRequestUri(), is("/requestPath?paramName1=param1&paramName2=param2"));
+        assertThat(sut.getRequestUri(), is(delegate.getRequestUri()));
     }
     
     /**
-     * HTTPバージョンを取得できることを確認
+     * HTTPリクエストと同一のHTTPバージョンを取得できることを確認
      */
     @Test
     public void testGetHttpVersion() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
-        assertThat(sut.getHttpVersion(), is("HTTP/1.1"));
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getHttpVersion(), is(delegate.getHttpVersion()));
     }
 
     /**
-     * リクエストパラメータのMapを取得できることを確認
+     * HTTPリクエストと同一のリクエストパラメータのMapを取得できることを確認
      */
     @Test
     public void testGetParamMap() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest( new MockHttpRequest()
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest()
                 .setParam("paramName1", "param1-1", "param1-2")
-                .setParam("paramName2", "param2"));
-        Map<String, String[]> result = sut.getParamMap();
-        assertEquals(result.size(), 2);
-        assertArrayEquals(result.get("paramName1"), new String[]{"param1-1", "param1-2"});
-        assertArrayEquals(result.get("paramName2"), new String[]{"param2"});
+                .setParam("paramName2", "param2");
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getParamMap(), is(delegate.getParamMap()));
     }
 
     /**
-     * リクエストパラメータを取得できることを確認
+     * HTTPリクエストと同一のリクエストパラメータを取得できることを確認
      */
     @Test
     public void testGetParam() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest()
-                .setParam("paramName", "param1", "param2"));
-        assertArrayEquals(sut.getParam("paramName"), new String[]{"param1", "param2"});
-
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest()
+                .setParam("paramName", "param1", "param2");
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getParam("paramName"), is(delegate.getParam("paramName")));
     }
 
     /**
-     * HTTPリクエストから指定したnameのパラメータ（配列）の先頭要素が取得できること
+     * HTTPリクエストから指定したnameのパラメータ（配列）の先頭要素が取得できることを確認
      */
     @Test
     public void testGetPathParam_Success () {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest()
-                .setParam("paramName", "param1", "param2"));
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest().setParam("paramName", "param1", "param2");
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         assertThat(sut.getPathParam("paramName"),  CoreMatchers.is("param1"));
     }
 
     /**
-     * HTTPリクエストで指定したnameのパラメータ（配列）が空だった場合、nullが返却されること
+     * HTTPリクエストで指定したnameのパラメータ（配列）が空だった場合、nullが返却されることを確認
      */
     @Test
     public void testGetPathParam_ParamIsEmpty () {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest().setParam("paramName"));
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest().setParam("paramName");
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         assertThat(sut.getPathParam("paramName"),  nullValue());
     }
 
     /**
-     * 指定したnameのパラメータがHTTPリクエストに存在しない場合、nullが返却されること
+     * 指定したnameのパラメータがHTTPリクエストに存在しない場合、nullが返却されることを確認
      */
     @Test
     public void testGetPathParam_ParamIsNotExist () {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         assertThat(sut.getPathParam("paramName"),  nullValue());
     }
 
     /**
-     * リクエストパラメータを設定できることを確認
+     * HTTPリクエストにリクエストパラメータを設定できることを確認
      */
     @Test
     public void testSetParam() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         assertThat(sut.getParamMap().size(), is(0));
         sut.setParam("paramName", "param1");
-        assertArrayEquals(sut.getParam("paramName"), new String[]{"param1"});
+        assertThat(sut.getParamMap().size(), is(1));
+        assertThat(sut.getParam("paramName"), is(delegate.getParam("paramName")));
     }
 
     /**
-     * リクエストパラメータのMapを設定できることを確認
+     * HTTPリクエストにリクエストパラメータのMapを設定できることを確認
      */
     @Test
     public void testSetParamMap() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         assertThat(sut.getParamMap().size(), is(0));
-        final Map<String, String[]> params = new HashMap<String, String[]>() {{
+        sut.setParamMap(new HashMap<String, String[]>() {{
             put("paramName1", new String[]{"param1-1", "param1-2"});
-        }};
-        sut.setParamMap(params);
-        assertThat(sut.getParamMap(), sameInstance(params));
+        }});
+        assertThat(sut.getParamMap().size(), is(1));
+        assertThat(sut.getParamMap(), is(delegate.getParamMap()));
     }
 
     /**
-     * ヘッダのMapを取得できることを確認
+     * HTTPリクエストと同一のヘッダのMapを取得できることを確認
      */
     @Test
     public void testGetHeaderMap() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest()
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest()
                 .setHeaderMap(new HashMap<String, String>() {{
                     put("key", "value");
-                }}));
-        assertThat(sut.getHeaderMap().get("key"), is("value"));
+                }});
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getHeaderMap(), is(delegate.getHeaderMap()));
     }
 
     /**
-     * ヘッダを取得できることを確認
+     * HTTPリクエストと同一のヘッダを取得できることを確認
      */
     @Test
     public void testGetHeader() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest()
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest()
                 .setHeaderMap(new HashMap<String, String>() {{
                     put("key", "value");
-                }}));
-        assertThat(sut.getHeader("key"), is("value"));
+                }});
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getHeader("key"), is(delegate.getHeader("key")));
     }
 
     /**
-     * HTTPリクエストのホストヘッダを取得できることを確認
+     * HTTPリクエストと同一のホストヘッダを取得できることを確認
      */
     @Test
     public void testGetHost() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest().setHost("host"));
-        assertThat(sut.getHost(), is("host"));
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest().setHost("host");
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getHost(), is(delegate.getHost()));
     }
 
     /**
-     * Cookieを取得できることを確認
+     * HTTPリクエストと同一のCookieを取得できることを確認
      */
     @Test
     public void testGetCookie() {
         MockHttpCookie cookie = new MockHttpCookie();
         cookie.put("key", "value");
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest().setCookie(cookie));
-        assertThat(sut.getCookie().get("key"), is("value"));
+        MockHttpRequest delegate = (MockHttpRequest) new MockHttpRequest().setCookie(cookie);
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getCookie(), is(delegate.getCookie()));
     }
 
     /**
-     * マルチパートの一部を取得できることを確認
+     * HTTPリクエストと同一のマルチパートの一部を取得できることを確認
      */
     @Test
     public void testGetPart() {
-        final MockHttpRequest request = new MockHttpRequest();
-        request.setMultipart(new HashMap<String, List<PartInfo>>() {{
+        MockHttpRequest delegate = new MockHttpRequest();
+        delegate.setMultipart(new HashMap<String, List<PartInfo>>() {{
             put("key", new ArrayList<PartInfo>() {{
                 add(PartInfo.newInstance("name"));
             }});
         }});
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(request);
-        List<PartInfo> result = sut.getPart("key");
-        assertThat(result.size(), is(1));
-        assertThat(result.get(0).getName(), is("name"));
-
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getPart("key"), is(delegate.getPart("key")));
     }
 
     /**
-     * マルチパートを設定できることを確認
+     * HTTPリクエストのマルチパートを設定できることを確認
      */
     @Test
     public void testSetMultipart() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
         assertThat(sut.getMultipart().size(), is(0));
         sut.setMultipart(new HashMap<String, List<PartInfo>>() {{
             put("key", new ArrayList<PartInfo>() {{
                 add(PartInfo.newInstance("name"));
             }});
         }});
-        Map<String, List<PartInfo>> result = sut.getMultipart();
-        assertThat(result.size(), is(1));
-        assertThat(result.get("key").size(), is(1));
-        assertThat(result.get("key").get(0).getName(), is("name"));
+        assertThat(sut.getMultipart().size(), is(1));
+        assertThat(sut.getMultipart(), is(delegate.getMultipart()));
     }
 
     /**
-     * 本HTTPリクエストの全マルチパートを取得できることを確認
+     * HTTPリクエストと同一の全マルチパートを取得できることを確認
      */
     @Test
     public void testGetMultipart() {
-        MockHttpRequest request = new MockHttpRequest();
-        request.setMultipart(new HashMap<String, List<PartInfo>>() {{
+        MockHttpRequest delegate = new MockHttpRequest();
+        delegate.setMultipart(new HashMap<String, List<PartInfo>>() {{
             put("key", new ArrayList<PartInfo>() {{
                 add(PartInfo.newInstance("name"));
             }});
         }});
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(request);
-        Map<String, List<PartInfo>> result = sut.getMultipart();
-        assertThat(result.size(), is(1));
-        assertThat(result.get("key").size(), is(1));
-        assertThat(result.get("key").get(0).getName(), is("name"));
-
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getMultipart(), is(delegate.getMultipart()));
     }
 
     /**
-     * UserAgent情報を取得できることを確認
+     * HTTPリクエストと同一のUserAgent情報を取得できることを確認
      */
     @Test
     public void testGetUserAgent() {
-        JaxRsHttpRequest sut = new JaxRsHttpRequest(new MockHttpRequest());
-        UserAgent result  = sut.getUserAgent();
-        assertThat(result.getBrowserType(), is("UnknownType"));
-        assertThat(result.getBrowserName(), is("UnknownName"));
-        assertThat(result.getBrowserVersion(), is("UnknownVersion"));
-        assertThat(result.getOsType(), is("UnknownType"));
-        assertThat(result.getOsName(), is("UnknownName"));
-        assertThat(result.getOsVersion(), is("UnknownVersion"));
+        MockHttpRequest delegate = new MockHttpRequest();
+        JaxRsHttpRequest sut = new JaxRsHttpRequest(delegate);
+        assertThat(sut.getUserAgent().getBrowserType(), is(delegate.getUserAgent().getBrowserType()));
+        assertThat(sut.getUserAgent().getBrowserName(), is(delegate.getUserAgent().getBrowserName()));
+        assertThat(sut.getUserAgent().getBrowserVersion(), is(delegate.getUserAgent().getBrowserVersion()));
+        assertThat(sut.getUserAgent().getOsType(), is(delegate.getUserAgent().getOsType()));
+        assertThat(sut.getUserAgent().getOsName(), is(delegate.getUserAgent().getOsName()));
+        assertThat(sut.getUserAgent().getOsVersion(), is(delegate.getUserAgent().getOsVersion()));
     }
 }
